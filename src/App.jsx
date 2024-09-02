b4f1f1b9e1878c9a51c38797a7db094b32040960
meetgadhiya077 <166481331+meetgadhiya077@users.noreply.github.com>
2024-09-02 15:40:15 +0530

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,7 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App = () => {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route index element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Home />;
 };
 
 export default App;
