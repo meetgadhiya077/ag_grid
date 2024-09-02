5a45fc71edb8488f277588907ba024134ee11fcf
meetgadhiya077 <166481331+meetgadhiya077@users.noreply.github.com>
2024-09-02 16:07:42 +0530

    Add files via upload

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -21,7 +21,8 @@ const Home = () => {
         time: item.Time,
         media: item.Media,
         input: item.Input,
-        output: item.Output
+        output: item.Output,
+        stage: item?.Stage
       }));
       setData(data);
       if (data?.length !== 0) {
@@ -44,7 +45,7 @@ const Home = () => {
 
           return acc;
         }, []);
-        getStage(messagesByName);
+        // getStage(messagesByName);
       }
     } catch (error) {
       console.log(error);
@@ -54,49 +55,49 @@ const Home = () => {
     }
   };
 
-  const getStage = async body => {
-    try {
-      setIsLoading(true);
-      const res = await Promise.all(
-        body?.map(data =>
-          axios({
-            method: 'POST',
-            url: 'https://api.openai.com/v1/chat/completions',
-            headers: {
-              Authorization:
-                'Bearer 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'
-            },
-            data: {
-              model: 'gpt-4o-mini',
-              messages: [
-                {
-                  role: 'system',
-                  content:
-                    'You are a helpful assistant. And you have to give the only answer Hot or Warm as per question.'
-                },
-                {
-                  role: 'user',
-                  content: `Analyze the given question and give the answer Hot if in a question user want to buy the product else if it's a normal conversation then give me Warm as a answer. Question: ${data.msg}`
-                }
-              ]
-            }
-          })
-        )
-      );
+  // const getStage = async body => {
+  //   try {
+  //     setIsLoading(true);
+  //     const res = await Promise.all(
+  //       body?.map(data =>
+  //         axios({
+  //           method: 'POST',
+  //           url: 'https://api.openai.com/v1/chat/completions',
+  //           headers: {
+  //             Authorization:
+  //               'Bearer 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'
+  //           },
+  //           data: {
+  //             model: 'gpt-4o-mini',
+  //             messages: [
+  //               {
+  //                 role: 'system',
+  //                 content:
+  //                   'You are a helpful assistant. And you have to give the only answer Hot or Warm as per question.'
+  //               },
+  //               {
+  //                 role: 'user',
+  //                 content: `Analyze the given question and give the answer Hot if in a question user want to buy the product else if it's a normal conversation then give me Warm as a answer. Question: ${data.msg}`
+  //               }
+  //             ]
+  //           }
+  //         })
+  //       )
+  //     );
 
-      setData(prevState => {
-        return prevState?.map(client => {
-          const index = body?.findIndex(data => data.name === client.name);
-          return { ...client, stage: res[index].data.choices[0].message.content };
-        });
-      });
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setIsLoading(false);
-      setisLoadingCSVData(true);
-    }
-  };
+  //     setData(prevState => {
+  //       return prevState?.map(client => {
+  //         const index = body?.findIndex(data => data.name === client.name);
+  //         return { ...client, stage: res[index].data.choices[0].message.content };
+  //       });
+  //     });
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setIsLoading(false);
+  //     setisLoadingCSVData(true);
+  //   }
+  // };
 
   useEffect(() => {
     getConversations();
